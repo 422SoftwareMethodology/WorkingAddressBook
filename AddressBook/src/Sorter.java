ec7f56404c6c62dd28f4734b868ad7cac4aa4f3c
Neekberm <NathanialJBoyd@gmail.com>
2017-02-02 17:20:24 -0800

    Resolves Ties

--- AddressBook/src/Sorter.java
+++ AddressBook/src/Sorter.java
@@ -7,9 +7,27 @@ import java.util.Comparator;
 
 public class Sorter {
 
-	public static ArrayList<Contact> sortByLastname(ArrayList<Contact> sortArrayList) {
-
-		Collections.sort(sortArrayList);
+public static ArrayList<Contact> sortByLastname(ArrayList<Contact> sortArrayList) {
+		
+		for (int i = 0; i < sortArrayList.size(); i++){
+			
+			Contact contact = sortArrayList.get(i);
+			String lastname = contact.get_lastName();
+			lastname = lastname.substring(0,1).toUpperCase() + lastname.substring(1);
+			contact.set_lastName(lastname);
+			
+		}
+		
+		Collections.sort(sortArrayList, new Comparator<Contact>() {
+		       public int compare(Contact c1, Contact c2) {
+		            int result =  c1.get_lastName().compareToIgnoreCase(c2.get_lastName());
+		            if (result != 0){
+		                //System.out.println("Matching Last name!");
+		            	return result;
+		                }
+		            return c1.get_firstName().compareToIgnoreCase(c2.get_firstName());
+		       }
+		   });
 
 		return sortArrayList;
 	}
